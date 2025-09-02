d5f85abf62c81207ee555261641893df06efd801
Acers <karthik.kmk.krishna@gmail.com>
2025-09-02 12:17:05 +0530

    forloops

---
+++ 7_forloops/1_forloop.js
@@ -0,0 +1,79 @@
+// print a number from 1-100
+
+// for(let i = 0 ;i<=100 ;i ++){
+//     console.log(i);
+    
+// }
+
+
+// for(let i=0;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+// for(i=0;i<=10;i++){
+//     console.log(5*i);
+    
+// }
+
+// for(let i=10;i>=0;i--){
+//     console.log(i);
+    
+// }
+
+// let sum = 0
+// for(let i=0;i<=50;i++){
+//     sum+=i
+// }
+// console.log(sum);
+
+
+// print the first 10 sqaures
+
+// for(let i=0;i<=10;i++){
+//     console.log(i ** 2);
+    
+// }
+
+
+// let fact = 1
+// for(let i=1;i<=5;i++){
+//     fact=fact*i
+// }
+
+// console.log(fact);
+
+// let arr = [10,20,30,40]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+    
+// }
+
+
+// largest number in an array
+// let arr = [1,20,3,23,56]
+// let max = arr[0]
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max=arr[i]
+//     }
+// }
+
+// console.log(max);
+
+
+// let arr = [1,1,2,3,1,1]
+// let target = 1
+// let count = 0
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]==target){
+//         count++
+//     }
+// }
+
+// console.log(count);
