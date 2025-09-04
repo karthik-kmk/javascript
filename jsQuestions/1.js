89f6747970fe5e820e0df6c047ca80903123e221
Acers <karthik.kmk.krishna@gmail.com>
2025-09-04 11:19:03 +0530

    questions

---
+++ jsQuestions/1.js
@@ -0,0 +1,105 @@
+// basics
+
+
+// log name and fav hobby
+// 1)
+
+// console.log("my name is karthik and my fav hobby is coding ");
+
+// 2)
+
+// let a = 45
+// let b = 2
+// let c = 10
+
+// let result  = a*b-c
+// console.log(result);
+
+
+// 3)
+
+// const newDate = new Date()
+// console.log(newDate.getFullYear());
+
+
+// 4)
+
+// let firstName = "Karthik"
+// let lastName = " Krishna"
+
+// console.log(firstName + lastName);
+
+
+// 5)
+// let a = 10
+// console.log(a);
+// a++
+// console.log(a);
+
+
+// console.error("something went wrong");
+
+// let a = 12
+// let result = 12 ** 2
+// console.log(result);
+
+
+// let boolean = true
+// console.log(typeof boolean);
+
+
+// let age = 18
+
+// if(age>18){
+//     console.log("greater than 18");
+    
+// }else{
+//     console.log("not greater than 18");
+    
+// }
+
+
+// console.log(100/0);
+
+
+
+// basics - 2
+//  let number = 10
+//  console.log(number);
+ 
+
+// const pi = Math.PI
+// console.log(pi);
+
+
+// let string = "karthik"
+// console.log(string);
+
+// string = "krishna"
+// console.log(string);
+
+
+// console.log(typeof null);
+
+// let string = "25"
+// console.log(typeof string);
+
+// let a = true
+// console.log(typeof a);
+
+
+// let string = "karthik"
+// let number = 10
+// let boolean = true
+
+// console.log(string,number,boolean)
+
+
+
+// let a
+// console.log(a);
+
+
+const arr = [10,20,30]
+arr = [20]
+console.log(arr);
